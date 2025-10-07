47cdef347426f472ff8c6c0f78d23d4d798d6a29
dpmorales7 <112967576+dpmorales7@users.noreply.github.com>
2025-10-07 13:48:09 -0400

    Update Feedback.java so that, Feedback methods have been completed to give full feedback on guesses made by user.
    isCorrect() returns true or false depending on the hashmap created by getPattern()

--- Feedback.java
+++ Feedback.java
@@ -3,4 +3,83 @@ public class Feedback {
 
 	
 	
+	public static HashMap getPatter(String guess, String secretWord) {
+		
+		HashMap<String, String> guesses = new HashMap<>();
+		
+		String colors = compare(guess,secretWord);
+		
+		guesses.put(guess, colors);
+		
+		
+		
+		
+		
+		
+		
+		
+		return guesses;
+		
+	}
+	
+	
+	public static String compare(String guess, String secretWord) {
+		
+		char[] letters = new char[5];
+		
+		for(int i = 0; i <5;i++) {
+			for(int j = 0; j<5; j++) {
+				
+				if(guess.charAt(i) == secretWord.charAt(j)) {
+					
+					letters[i] = 'Y';
+					
+				}
+			
+				
+			}
+		}
+		
+		for(int k = 0; k < 5; k++) {
+			if (guess.charAt(k) == secretWord.charAt(k)) {
+				letters[k] = 'G';
+				
+			}
+	
+		}
+		
+		for(int l = 0; l < 5; l++) {
+			
+			if (letters[l] == '\0') {
+				letters[l] = 'B';
+				
+			}
+		}
+		
+		String colorChars = new String(letters);
+		
+		
+		return colorChars ;
+		
+		
+	}
+
+	public static boolean isCorrect(HashMap guesses, String guess){
+
+		String colors = guesses[guess];
+
+		if (colors == "GGGGG"){
+			return True;
+		}
+
+		return False;
+		
+		
+	}
+
+	
+
+	
+	
 }
+
